7c7dd61b121c464912dfcb90858953297ef2304e
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-08-28 01:38:42 +0200

    Completed backend

--- src/clients/person-client/index.ts
+++ src/clients/person-client/index.ts
@@ -1,7 +1,7 @@
 import { handleApiCall } from '../../utils/helpers';
 import { PEOPLE_URL } from '../constants';
 
-export default async function handleGetPersonClient(name: String) {
+export default async function handleGetPersonClient(name: string) {
   const {
     data: { results },
   } = await handleApiCall(`${PEOPLE_URL}?search=${name}`);

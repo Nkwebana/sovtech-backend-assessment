013cb246b35523480e2eb5563d7b0536d596696f
Buyisile Nkwebana <buyisilenkwebana@Buyisiles-MacBook-Air.local>
2021-08-28 03:01:44 +0200

    Code cleanup.

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,7 +1,6 @@
 import handleGetPeopleClient from './get-people-client';
 import handleSearchPersonClient from './search-person-client';
 import handleGetPersonHomeWorldClient from './get-home-world-client';
-('./home-world-client');
 
 export {
   handleGetPeopleClient,
